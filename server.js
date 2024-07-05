5c364eb9d0c20572e29f5a77f802b960a930a54e
Maria Eduarda Vasconcelos <eduuardavasconcelos@gmail.com>
2024-07-05 16:05:51 -0300

    suporte CORS

--- server.js
+++ server.js
@@ -3,8 +3,17 @@ const server = jsonServer.create();
 const router = jsonServer.router('db.json');
 const middlewares = jsonServer.defaults();
 
+// Adicionar suporte a CORS
+server.use((req, res, next) => {
+  res.header('Access-Control-Allow-Origin', '*');
+  res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
+  res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
+  next();
+});
+
 server.use(middlewares);
 server.use(router);
-server.listen(3001, () => {
-  console.log('JSON Server is running')
+const PORT = process.env.PORT || 3001;
+server.listen(PORT, () => {
+  console.log(`JSON Server is running on port ${PORT}`);
 });

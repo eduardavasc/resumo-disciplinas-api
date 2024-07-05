6e91928bc446d211bbcf2efd97c1906e5e69900e
Maria Eduarda Vasconcelos <eduuardavasconcelos@gmail.com>
2024-07-05 16:06:18 -0300

    adicionando suporte ao CORS

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const server = jsonServer.create();
 const router = jsonServer.router('db.json');
 const middlewares = jsonServer.defaults();
 
-// Adicionar suporte a CORS
+
 server.use((req, res, next) => {
   res.header('Access-Control-Allow-Origin', '*');
   res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
